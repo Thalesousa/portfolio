49573dc4344e4ed293ad8e008768bd8cbd7487a2
Thalesousa <thalestjsb@gmail.com>
2021-10-01 22:13:57 -0300

    remove blank link  Footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -12,9 +12,9 @@ export function Footer({ year }: FooterProps) {
       <span>E ai, vamos trabalhar juntos? <span>😃</span></span>
       <ul>
         <li><span>&#169;{year} Thales Sousa</span></li>
-        <li><a href="https://www.linkedin.com/in/thalesousa/" target="_blank"><AiFillLinkedin /></a></li>
-        <li><a href="https://github.com/Thalesousa" target="_blank"><AiFillGithub /></a></li>
-        <li><a href="https://codepen.io/thalesousa" target="_blank"><AiFillCodepenCircle /></a></li>
+        <li><a href="https://www.linkedin.com/in/thalesousa/"><AiFillLinkedin /></a></li>
+        <li><a href="https://github.com/Thalesousa"><AiFillGithub /></a></li>
+        <li><a href="https://codepen.io/thalesousa"><AiFillCodepenCircle /></a></li>
       </ul>
     </footer>
   )
